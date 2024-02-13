c7281f1229ae6111794152661e4cdd9f638954d3
Crowdbotics <team@crowdbotics.com>
2024-02-13 16:14:30 +0000

    Initial commit

---
+++ backend/solar_panel_vis_46300/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for solar_panel_vis_46300 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "solar_panel_vis_46300.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "solar-panel-vis-46300",
+  url: "https://solar-panel-vis-46300.botics.co",
+  api: "https://solar-panel-vis-46300.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
